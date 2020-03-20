002ba19ae66d077fc2f3cc229b009b809ef2633a
mcfreund <m.freund@wustl.edu>
2020-03-20 00:32:37 -0500

    created define_subj_samples.R

---
+++ src/define_rois.R
@@ -0,0 +1,41 @@
+## about ----
+## 
+## 
+## mike freund, 2020-03-19
+
+
+## setup ----
+
+library(here)
+library(magrittr)
+library(gifti)
+library(cifti)
+library(abind)
+library(data.table)
+library(mikeutils)
+library(progress)
+library(profvis)
+
+## variables
+
+parcellation <- read_atlas("schaefer400")
+
+dir.ccp.hcp <- "/data/nil-bluearc/ccp-hcp/DMCC_ALL_BACKUPS/HCP_SUBJECTS_BACKUPS/AFNI_ANALYSIS"
+dir.subsubj <- "/data/nil-external/ccp/witzermanm/AFNI_ANALYSIS_SUBSUBJECT"
+dir.results <- file.path(dir.subsubj, "RESULTS_RUNWISE")
+
+subjs <- list.dirs(dir.results, recursive = FALSE, full.names = FALSE)
+
+r.vn <- readRDS(here("out", "rsa", "rmatrix_vanilla_shaefer400.rds"))  ## vanilla RSA
+# str(r.vn)
+# sessi <- "baseline"
+# sessi.short <- "Bas"
+# n.knots <- 6
+# glms <- "Congruency_EVENTS_censored"
+# regressors <- c("PC50InCon", "biasInCon", "PC50Con", "biasCon")
+# measures <- c("corr", "eucl")
+# normalizations <- c("raw", "prw")
+# rsatypes <- c("vanilla", "crossval")
+
+
+## ----
