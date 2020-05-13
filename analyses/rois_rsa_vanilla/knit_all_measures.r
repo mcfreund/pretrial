bfe942df204b287442a6a7d4d5e4f53fd6a29f99
mcfreund <m.freund@wustl.edu>
2020-05-13 11:35:29 -0500

    big commit

--- analyses/rois_rsa_vanilla/knit_all_measures.r
+++ analyses/rois_rsa_vanilla/knit_all_measures.r
@@ -5,13 +5,19 @@ wd <- here("analyses", "rois_rsa_vanilla")
 
 for (measure.i in c("corr", "eucl", "neuc")) {
   
-  dir.create(file.path(wd, measure.i))
-  
-  render(
-    input = file.path(wd, "rois_rsa_vanilla.rmd"),
-    output_file = file.path(wd, paste0("rois_rsa_vanilla_", measure.i, ".html")),
-    knit_root_dir = file.path(wd, measure.i)
-  )
+  for (norma.i in c("raw", "prw")) {
+    
+    # if (paste0(measure.i, "_", norma.i) %in% c("corr_raw", "corr_prw", "eucl_raw")) next
+    
+    dir.create(file.path(wd, paste0(measure.i, "_", norma.i)))
+    
+    render(
+      input = file.path(wd, "rois_rsa_vanilla.rmd"),
+      output_file = file.path(wd, paste0("rois_rsa_vanilla_", measure.i, "_", norma.i, ".html")),
+      knit_root_dir = file.path(wd, paste0(measure.i, "_", norma.i))
+    )
+    
+  }
   
 }
 
